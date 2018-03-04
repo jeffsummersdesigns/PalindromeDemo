804c6f752095c30839c0c17dec19b26b1fde57c8
jeffsummersdesigns <jeff-summers@live.com>
2018-03-04 14:06:10 -0800

    Committed changes locally before pushing to repo.

---
+++ PalindromeDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PalindromeDemo
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Please enter a word to check if it is a palindrome.");
+            String phrase = Console.ReadLine();
+
+            if (IsAPalindrome(phrase))
+                Console.WriteLine("It is a palindrome!");
+            else
+                Console.WriteLine("It is not a palindrome!");
+
+            Console.ReadLine();
+
+        }
+        
+        public static bool IsAPalindrome(String phrase) { 
+        
+            // get the size of the phrase
+            int length = phrase.Length;
+            
+            // count or cursor. Initilize as 0 
+            int c = 0;
+
+            while (c <= length / 2)
+            {
+                // start at 0 and compare to value on last index,
+                // increment by 1 from both sides and compare values
+                if (phrase[c] != phrase[phrase.Length - 1 - c])
+                    return false;
+                    c++;
+            }
+            return true;
+
+        }   
+    }
+}
